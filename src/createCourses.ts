eb32e9e91e7014ccb7297b4e943aa6eeb52e1672
MBahyeldin <muhammad.baheuddeen@gmail.com>
2026-02-16 15:23:29 +0200

    catch errors

--- src/createCourses.ts
+++ src/createCourses.ts
@@ -25,64 +25,67 @@ async function createCourses() {
     try {
         for (let  c of flattenedModules) {
             console.log(c.course_name);
-            
-                const course = await zohoAgent.getOrCreateCourse({
-                    name: c.course_name,
-                    description: c.course_name
-                });
+                try {            
+                    const course = await zohoAgent.getOrCreateCourse({
+                        name: c.course_name,
+                        description: c.course_name
+                    });
 
-                for (let section of c.sections.sort((a, b) => a.section_number - b.section_number)) {
-                    const chapter = await zohoAgent.addChapterToCourse({
-                        course,
-                        name: section.section_name
-                    });  
-
-                    for (let module of section.modules) {
-                        let moduleContentType = "";
-                        await new Promise((res) => {
-                            setTimeout(() => res(true), 3000);
-                        });
-                        switch (module.module_content_type) {
-                            case "TEXT":
-                                if (module.isUrl) {
-                                    moduleContentType = "TEXT-URL"
-                                } else {
-                                    moduleContentType = "TEXT-IMAGE"
-                                }
-                                break
-                            case "DOCUMENT":
-                                moduleContentType = "DOCUMENT";
-                                break;
-                            case "VIDEO":
-                                if (module.onSharePoint && !module.isDeleted && module.module_content != "-1") {
-                                    moduleContentType = "VIDEO-VIMEO"
-                                } else if (module.onYouTube) {
-                                    moduleContentType = "VIDEO-YOUTUBE"
-                                }
-                                break;
-                        }
-
-                        if (module.module_content_type == "UNKNOWN" || !moduleContentType) {
-                            console.log("skipping: ", module.module_content_type, moduleContentType);
-                            
-                            continue;
-                        }
-
-                        const lesson = await zohoAgent.addLessonToChapter({
+                    for (let section of c.sections.sort((a, b) => a.section_number - b.section_number)) {
+                        const chapter = await zohoAgent.addChapterToCourse({
                             course,
-                            chapter,
-                            name: module.module_name,
-                            type: module.module_content_type
-                        });
+                            name: section.section_name
+                        });  
 
-                        await zohoAgent.setLessonContent({
-                            course,
-                            lesson,
-                            type: moduleContentType,
-                            content: module.module_content
-                        })
+                        for (let module of section.modules) {
+                            let moduleContentType = "";
+                            await new Promise((res) => {
+                                setTimeout(() => res(true), 3000);
+                            });
+                            switch (module.module_content_type) {
+                                case "TEXT":
+                                    if (module.isUrl) {
+                                        moduleContentType = "TEXT-URL"
+                                    } else {
+                                        moduleContentType = "TEXT-IMAGE"
+                                    }
+                                    break
+                                case "DOCUMENT":
+                                    moduleContentType = "DOCUMENT";
+                                    break;
+                                case "VIDEO":
+                                    if (module.onSharePoint && !module.isDeleted && module.module_content != "-1") {
+                                        moduleContentType = "VIDEO-VIMEO"
+                                    } else if (module.onYouTube) {
+                                        moduleContentType = "VIDEO-YOUTUBE"
+                                    }
+                                    break;
+                            }
+
+                            if (module.module_content_type == "UNKNOWN" || !moduleContentType) {
+                                console.log("skipping: ", module.module_content_type, moduleContentType);
+                                
+                                continue;
+                            }
+
+                            const lesson = await zohoAgent.addLessonToChapter({
+                                course,
+                                chapter,
+                                name: module.module_name,
+                                type: module.module_content_type
+                            });
+
+                            await zohoAgent.setLessonContent({
+                                course,
+                                lesson,
+                                type: moduleContentType,
+                                content: module.module_content
+                            })
+                        }
                     }
-                }
+                } catch (error) {
+                console.log('Error processing course:', c.course_name, error);
+            }
         }
     } catch (error) {
         console.log('Error normalizing data:', error);
